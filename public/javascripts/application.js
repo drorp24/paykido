91ddd9adf14117752e1ef1cce80a00c4896e9b68
Dror Poliak <Dror@.(none)>
2010-07-22 20:11:45 +0300

    CONSUMER UNIFIED UNDER ONE TAB

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -47,7 +47,7 @@ $(document).ready(function (){
 			});
 		});
 		$(function() {
-			$("#tabs").tabs().find(".ui-tabs-nav").sortable({axis:'x'});
+			$("#tabs, #consumer_tabs").tabs().find(".ui-tabs-nav").sortable({axis:'x'});
 		});
 	
 		$(function() {
