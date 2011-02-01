f91e2f8dba7eabb56e58a4c0ff7bad82e91590ae
Dror Poliak <Dror@.(none)>
2011-02-01 16:35:51 +0200

    .

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -55,7 +55,7 @@ class Consumer < ActiveRecord::Base
   def self.payer_consumers_the_works(payer_id)
 
     self.find_all_by_payer_id(payer_id,
-               :group => ("consumers.id, consumer.name, authorized"),
+               :group => ("consumers.id, consumers.name, authorized"),
                :select => "consumers.id, name, balance, billing_phone, pin, pic, payer_rules.id as payer_rule_id, allowance, rollover, auto_authorize_under, auto_deny_over, authorized, sum(amount) as sum_amount",
                :joins => "inner join payer_rules on consumers.id = payer_rules.consumer_id LEFT OUTER JOIN purchases on consumers.id = purchases.consumer_id",
                :order => "consumers.updated_at desc")
