ed62af5addeee65d452616a50fe65c4e0db6535e
Dror Poliak <Dror@.(none)>
2011-02-02 15:46:45 +0200

    c

--- config/routes.rb
+++ config/routes.rb
@@ -70,6 +70,6 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-  map.root :controller => 'service'
-  map.home '', :controller => 'foo', :action => 'index'
+  map.root :controller => 'service', :action => 'index'
+  map.home '', :controller => 'service', :action => 'index'
 end
