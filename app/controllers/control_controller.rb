f2c888d8b760a36768310f7a22b5bf91567c2896
Dror Poliak <drorp24@yahoo.com>
2012-01-29 23:37:07 +0200

    New 'control' app based on Spina + facebooker.yml matches beta
    Signed-off-by: Dror Poliak <drorp24@yahoo.com>

---
+++ app/controllers/control_controller.rb
@@ -0,0 +1,69 @@
+
+class ControlController < ApplicationController
+
+  def index
+    
+  end
+  
+  def login
+    
+    if request.post?
+           
+      unless session[:user] = User.authenticate(params[:user][:email], params[:user][:password])
+        flash.now[:notice] = "user or password are incorrect. Please try again!"
+        return
+      end
+      
+#     set_payer_session
+      redirect_to :action => :index
+    end
+          
+  end
+
+  def dashboard
+    @purchases = Purchase.where("payer_id = 63").includes(:consumer, :retailer, :product, :category)
+    @pendings = Purchase.where("payer_id = ? and authorization_type = ?", 63, 'PendingPayer').count
+    @payer = Payer.find(63)
+    @consumer = Consumer.find(281)
+    session[:consumer] = @consumer
+  end
+  
+  def consumer_update
+    
+    find_consumer
+
+    if params[:consumer] 
+      if  params[:consumer][:allowance] or params[:consumer][:allowance_period]
+            @consumer.record_allowance_change
+      end
+      if @consumer.update_attributes!(params[:consumer])
+          session[:consumer] = @consumer
+      else
+          flash[:notice] = "Invalid... please try again!"
+          return
+      end
+    end
+        
+    respond_to do |format|  
+      format.js  
+    end
+    
+  end
+
+
+  private
+
+  def find_consumer
+    
+    if session[:consumer] and session[:consumer].id == params[:id]
+      @consumer = session[:consumer]
+     else
+      @consumer = Consumer.find(params[:id])
+    end
+   
+    session[:consumer] = @consumer
+
+  end
+  
+
+end
