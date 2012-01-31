7dc5610bfe8fe99a7e3a9b4afba76d58487a0d11
Dror Poliak <drorp24@yahoo.com>
2012-01-31 12:28:29 +0200

    www.paykido.com
    Signed-off-by: Dror Poliak <drorp24@yahoo.com>

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,15 @@
+class LandingController < ApplicationController
+
+#   before_filter   :check_friend_authenticated    
+
+  
+  def index 
+        
+  end
+  
+  def comingsoon
+    
+  end
+  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Arca::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'landing#index'
 
   # See how all your routes lay out with "rake routes"
 
