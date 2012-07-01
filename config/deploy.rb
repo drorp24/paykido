442efd6e2b4dcdaab58629f976880387e568ef29
Gady <gaaady@gmail.com>
2012-07-01 14:49:42 +0300

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:drorp/#{application}.git"
+set :repository, "git@github.com:drorp24/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
