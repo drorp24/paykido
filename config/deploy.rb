aeaa4f7b45dce3b9dcf6f2f75212869d6a46a341
Dror Poliak <drorp24@gmail.com>
2012-07-02 17:41:57 +0300

    fixed the damn deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,16 +12,23 @@ load "config/recipes/utils"
 server "50.116.63.241", :web, :app, :db, primary: true
 
 set :user, "deployer"
+set :password, "deployerpass2412"
+
 set :application, "paykido"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "https://github.com/drorp24/paykido.git"
+set :repository, "git@github.com:drorp24/paykido.git" #"https://github.com/drorp24/paykido.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+ssh_options[:paranoid] = false
+# ssh_options[:verbose] = :debug
+# ssh_options[:username] = 'deployer'
+# ssh_options[:host_key] = 'ssh-dss'
+
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
