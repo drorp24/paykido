e512041777532d94614c8aaf87a3fcffca411fcf
Gady <gaaady@gmail.com>
2012-07-10 17:16:01 +0300

    Fixed taps recpie

--- config/recipes/taps.rb
+++ config/recipes/taps.rb
@@ -3,7 +3,7 @@ namespace :taps do
   desc "Run a taps server"
   # run like: cap rake:invoke task=a_certain_task
   task :start, :roles => :db do
-    run("cd #{deploy_to}/current; taps server postgres://paykido@localhost/paykido tmpuser tmppass")
+    run("cd #{deploy_to}/current; bundle exec taps server postgres://#{postgresql_user}:#{postgresql_password}@localhost/#{postgresql_database} tapsuser tapspass")
   end
 
 end
