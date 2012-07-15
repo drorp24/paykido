d75c4202dd90b5efec61303ce17178c1eec52fe0
Dror Poliak <drorp24@gmail.com>
2012-07-16 01:46:18 +0300

    Signed-off-by: Dror Poliak <drorp24@gmail.com>

--- app/controllers/g2s_controller.rb
+++ app/controllers/g2s_controller.rb
@@ -10,7 +10,7 @@ class G2sController < ApplicationController
     if params[:customField1] == 'payment'
         redirect_to purchase_url(params[:customField2].to_i, params.except(:action, :controller))
     elsif params[:customField1] == 'registration'
-#        redirect_to register_payer_url(params[:customField2].to_i, params.except(:action, :controller))
+        redirect_to register_payer_url(params[:customField2].to_i, params.except(:action, :controller))
     else
       flash[:error] = ""
       redirect_to root_path
@@ -30,8 +30,7 @@ class G2sController < ApplicationController
       @purchase.create_transaction!(params)
       # notify/approve/inform      
     elsif params[:customField1] == 'registration'
-      a = 1 /0
-#      @payer.create_registration!(params)
+      @payer.create_registration!(params)
     else
       return false   
     end
