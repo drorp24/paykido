22575bc3b7b9704e0ae648cd7d493c7821cf804d
Dror Poliak <drorp24@gmail.com>
2012-07-15 22:45:45 +0300

    Signed-off-by: Dror Poliak <drorp24@gmail.com>

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -191,12 +191,12 @@ class Purchase < ActiveRecord::Base
 #      :sg_IPAddress  => request.remote_ip, 
       :sg_Email  => registration.Email,
       :sg_ClientUniqueID => self.id
-    })
+    }).inspect
     rescue => e
       @paid_by_token = false
       self.transactions.create!(:status => e)
     else
-      self.transactions.create!(:status => "ok")
+      self.transactions.create!(:Reason => token_response)
     end
      
 if false
