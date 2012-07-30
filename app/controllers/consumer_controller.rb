1cf1f61af97a84ec35d81e0307f9139a535f9c4f
Dror Poliak <drorp24@gmail.com>
2012-07-30 15:54:48 +0300

    Signed-off-by: Dror Poliak <drorp24@gmail.com>

--- app/controllers/consumer_controller.rb
+++ app/controllers/consumer_controller.rb
@@ -20,29 +20,45 @@ class ConsumerController < ApplicationController
     # A payer may already be associated with a consumer (e.g. above case)
     # A payer whose email was given may exist and not be linked to consumer (e.g. another brother joining) 
     
+    Rails.logger.debug("ENTERS REGISTER CALLBACK")  
+
     if facebook_params_user_id = facebook_params['user_id']
-      @consumer = Consumer.find_or_initialize_by_facebook_id(facebook_params_user_id) 
-      Rails.logger.info("Facebook params (signed request/facebooker) user id is: " + facebook_params_user_id.to_s)  
+
+      Rails.logger.debug("Facebook params (signed request/facebooker) user id is: " + facebook_params_user_id.to_s)  
       if c = Consumer.find_by_facebook_id(facebook_params_user_id)
-        Rails.logger.info("Consumer with that facebook id exists already. Its ID is: " + c.id.to_s)
+        Rails.logger.debug("Consumer with that facebook id exists already. Its id is: " + c.id.to_s)
       else
-        Rails.logger.info("No consumer currently exists that has that facebook id. Initialized record created")
+        Rails.logger.debug("No consumer currently exists with that facebook id. Initialized record created")
       end
+
+      @consumer = Consumer.find_or_initialize_by_facebook_id(facebook_params_user_id) 
+
     elsif current_facebook_user # probably never true 
-      Rails.logger.info("Facebook params (signed request/facebooker) passed no facebook user id, but current_Facebook_user exists")  
+
+      Rails.logger.debug("Facebook params (signed request/facebooker) passed no facebook user id, but current_Facebook_user exists")  
+
       @consumer = Consumer.find_or_initialize_by_facebook_id(current_facebook_user.id)
+
     else                        # this is actually an error 
-      Rails.logger.info("Facebook params (signed request/facebooker) passed no facebook user id. No current_Facebook_user")  
+
+      Rails.logger.debug("Facebook params (signed request/facebooker) passed no facebook user id. No current_Facebook_user")  
+
       @consumer = session[:consumer] || Consumer.new
+
     end 
     
     if @consumer.payer == nil 
-      Rails.logger.info("@consumer.payer at that point is empty")
+      Rails.logger.debug("@consumer.payer at that point is empty")
+    else
+      Rails.logger.debug("@consumer.payer already exists. Its id is: " + @consumer.payer_id.to_s)  
     end
-    if Payer.find_by_email(facebook_params['registration']['payer_email'])  
     
-
-    Rails.logger.info("At that point, @consumer.payer id is: " + @consumer.payer_id.to_s)  
+    if p = Payer.find_by_email(facebook_params['registration']['payer_email'])  
+      Rails.logger.debug("There is a payer with the form email of " + facebook_params['registration']['payer_email'])
+      Rails.logger.debug("Its id is: " + p.id.to_s)
+    else
+      Rails.logger.debug("There isnt any payer with the form email of " + facebook_params['registration']['payer_email'])
+    end   
 
     @payer = @consumer.payer || Payer.find_or_initialize_by_email(facebook_params['registration']['payer_email'])
     
@@ -54,6 +70,10 @@ class ConsumerController < ApplicationController
     @payer.password= "1"     # TEMP: till devise does it properly, payer's hashed_password is used as access token   
     @payer.save!
 
+    Rails.logger.debug("The id of the payer that was updated/created is: " + @payer.id.to_s)
+    Rails.logger.debug("The facebook param name is: " + facebook_params['registration']['payer_name'])
+    Rails.logger.debug("Accordingly, I updated payer name to: " + @payer.name)
+
     @consumer.update_attributes!(
           :name => facebook_params['registration']['name'].split(' ')[0],
           :payer_id => @payer.id, 
@@ -61,9 +81,14 @@ class ConsumerController < ApplicationController
           :pic =>  "https://graph.facebook.com/" + facebook_params['user_id'] + "/picture?type=large",
           :tinypic => "https://graph.facebook.com/" + facebook_params['user_id'] + "/picture")
    
+    Rails.logger.debug("Consumer is saved for the first time. Its id is: " + @consumer.id.to_s)
+    Rails.logger.debug("The payer_id inserted into the consumer is: " + @consumer.payer_id.to_s)
+    Rails.logger.debug("Consumer created_at got: " + @consumer.created_at.to_s)
 
     session[:consumer] = @consumer
     session[:payer] =    @payer
+    
+    Rails.logger.debug("EXISTS REGISTER CALLBACK")  
     
   end 
 
@@ -98,22 +123,42 @@ class ConsumerController < ApplicationController
     @purchase.notify_merchant(status) 
     @purchase.notify_consumer('programmatic', status)
 
-    redirect_to params[:referrer]  + '?status=' + status
+    redirect_to params[:referrer], 
+      :status =>    status, 
+      :property =>  @purchase.authorization_property,
+      :value =>     @purchase.authorization_value,
+      :type =>      @purchase.authoriztion_type
     
   end
   
   def find_consumer_and_payer
     
+    Rails.logger.debug("ENTERS BUY")  
+
+
     # ToDo: buy is no possible unless consumer has authorized (register_callback) Paykido
     # Consumer with that facebook id must exists after consumer registration. If not it's an error.
+
+    Rails.logger.debug("The facebook id I received as parameter is: " + params[:facebook_id].to_s)
+    if c = Consumer.find_by_facebook_id(params[:facebook_id])
+      Rails.logger.debug("Consumer with that facebook id was found. Its id is: " + c.id.to_s)
+    else
+      Rails.logger.debug("Consumer with that facebook id was NOT found. So I initialized one.")      
+    end  
+
     @consumer = Consumer.find_or_initialize_by_facebook_id(params[:facebook_id])
     @consumer.name = params[:name]
     @consumer.pic =  params[:pic]
     @consumer.save!
+
+    Rails.logger.debug("Updated consumer name and pic. Its id is: " + @consumer.id.to_s)      
     
     # ToDo: no session needed - delete
     session[:consumer] = @consumer
     @payer = session[:payer] = @consumer.payer unless @consumer.nil?
+    
+    Rails.logger.debug("The payer Im using is the consumer payer. Its id is: " + @payer.id.to_s)      
+
   end
 
 
