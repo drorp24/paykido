29a630a4341c403eac33d5361de27ec2f404a415
Dror Poliak <drorp24@gmail.com>
2012-08-12 13:54:43 +0300

    Signed-off-by: Dror Poliak <drorp24@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,7 @@ Paykido::Application.configure do
   #config.assets.css_compressor = :scss
 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.logger = nil
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'www.paykido.com' }
