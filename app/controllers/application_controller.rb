3892fc745f0aae44489af7e1207f4f7eff957e27
Dror Poliak <drorp24@gmail.com>
2012-08-14 20:28:18 +0300

    sign-in and sign-out
    Signed-off-by: Dror Poliak <drorp24@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,14 @@ class ApplicationController < ActionController::Base
 
   layout :set_layout
 
+  def after_sign_in_path_for(payer)
+    if current_payer.registered?
+      return purchases_path
+    else
+      return new_token_path
+    end
+  end
+
   private
 
 #  def set_locale
