0feb8350d2f83c02a796dcf361574927539012b8
Dror Poliak <drorp24@gmail.com>
2013-01-28 19:08:08 +0200

    .

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -71,7 +71,7 @@ Paykido::Application.configure do
   config.action_mailer.logger = nil
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'paykido-beta.herokuapp.com'  }
+  config.action_mailer.default_url_options = { :host => 'paykido-staging.herokuapp.com'  }
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
@@ -91,7 +91,7 @@ Paykido::Application.configure do
   }
 
 
-  config.action_mailer.default_url_options = { :host => 'paykido-beta.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'paykido-staging.herokuapp.com' }
   
   config.log_level = :debug
 
